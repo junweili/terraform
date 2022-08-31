876d58e9dc5d95c540e9bb9eddf2a8e84643d1a7
Junwei Li <junweili@users.noreply.github.com>
2022-08-31 16:02:44 -0600

    Create main.tf

---
+++ pr3021575-1/main.tf
@@ -0,0 +1,29 @@
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+    # The "feature" block is required for AzureRM provider 2.x. 
+    # If you're using version 1.x, the "features" block is not allowed.
+    version = "~>2.0"
+    features {}
+}
+
+# Create a resource group if it doesn't exist
+resource "azurerm_resource_group" "myterraformgroup" {
+    name     = var.rg_name
+    location = "eastus"
+
+    tags = {
+        environment = var.myTag
+    }
+}
+
+# Create virtual network
+resource "azurerm_virtual_network" "myterraformnetwork" {
+    name                = "myVnet"
+    address_space       = ["10.0.0.0/16"]
+    location            = "eastus"
+    resource_group_name = azurerm_resource_group.myterraformgroup.name
+
+    tags = {
+        environment = var.myTag
+    }
+}
