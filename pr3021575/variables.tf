f0c791e613c2d9e3bff7c87f74cc4d0a9cb70f13
Junwei Li <junweili@users.noreply.github.com>
2022-08-31 10:08:22 -0600

    Create variables.tf

---
+++ pr3021575/variables.tf
@@ -0,0 +1,47 @@
+variable "rg_name" {
+    type = string
+}
+
+variable "vnet_name" {
+    type = string
+}
+
+variable "rg_hub_name" {
+  type = string
+}
+
+variable "vnet_hub_name" {
+  type = string
+}
+
+variable "subscription_id" {
+    type = string
+}
+
+variable "client_id" {
+    type = string
+}
+
+variable "client_secret" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
+
+variable "subscription_id2" {
+    type = string
+}
+
+variable "client_id2" {
+    type = string
+}
+
+variable "client_secret2" {
+  type = string
+}
+
+variable "tenant_id2" {
+  type = string
+}
