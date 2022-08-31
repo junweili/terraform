88c35ce970a89026f255168372a5499d35e5af2d
Junwei Li <junweili@users.noreply.github.com>
2022-08-31 16:57:57 -0600

    Create variables.tf

---
+++ pr3021575-2/variables.tf
@@ -0,0 +1,14 @@
+variable "vm_name" {
+  description = "VM Name"
+  default     = "myVM"
+}
+
+variable "rg_name1" {
+  description = "Resource group Name"
+  default     = "junwei-rg1"
+}
+
+variable "rg_name2" {
+  description = "Resource group Name"
+  default     = "junwei-rg2"
+}
