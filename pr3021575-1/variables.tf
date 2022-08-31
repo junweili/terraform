b4c81ab25b9e69a6a6da4c30df153001b29ee9ae
Junwei Li <junweili@users.noreply.github.com>
2022-08-31 16:01:35 -0600

    Create variables.tf

---
+++ pr3021575-1/variables.tf
@@ -0,0 +1,14 @@
+variable "vm_name" {
+  description = "VM Name"
+  default     = "myVM"
+}
+
+variable "rg_name" {
+  description = "Resource group Name"
+  default     = "junwei-rg"
+}
+
+variable "myTag" {
+  description = "My Default Tag"
+  default = "terraform-test"
+}
