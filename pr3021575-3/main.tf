736c047118ccfc3d00878fb9b34adac96a162deb
Junwei Li <junweili@users.noreply.github.com>
2022-08-31 17:44:36 -0600

    Create main.tf

---
+++ pr3021575-3/main.tf
@@ -0,0 +1,30 @@
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+    # The "feature" block is required for AzureRM provider 2.x. 
+    # If you're using version 1.x, the "features" block is not allowed.
+    version = "~>2.0"
+    features {}
+    alias = "azure1"
+}
+
+provider "azurerm" {
+    # The "feature" block is required for AzureRM provider 2.x. 
+    # If you're using version 1.x, the "features" block is not allowed.
+    version = "~>2.0"
+    features {}
+    alias = "azure2"
+}
+
+# Create a resource group if it doesn't exist
+resource "azurerm_resource_group" "myterraformgroup1" {
+    name     = var.rg_name1
+    location = "eastus"
+    provider            = azurerm.azure1
+}
+
+# Create a resource group if it doesn't exist
+resource "azurerm_resource_group" "myterraformgroup2" {
+    name     = var.rg_name2
+    location = "westus2"
+    provider            = azurerm.azure2
+}
