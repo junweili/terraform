7f4fa9994dcf056349bbd1f7f0b7e9500424968b
Junwei Li <junweili@users.noreply.github.com>
2022-08-31 23:54:57 -0600

    Update main.tf

--- pr3021575-4/main.tf
+++ pr3021575-4/main.tf
@@ -40,7 +40,7 @@ resource "azurerm_resource_group" "myterraformgroup2" {
 # Create virtual network
 resource "azurerm_virtual_network" "myterraformnetwork2" {
     name                = "myVnet2"
-    location            = "westus2"
     address_space       = ["10.0.0.0/16"]
+    location            = "westus2"
     resource_group_name = azurerm_resource_group.myterraformgroup2.name
 }
