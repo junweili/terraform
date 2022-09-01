a788b47d8fd72989d58cb97669edf9ec68f9eedc
Junwei Li <junweili@users.noreply.github.com>
2022-08-31 23:17:35 -0600

    Create main.tf

---
+++ pr3021575-4/main.tf
@@ -0,0 +1,46 @@
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+    # The "feature" block is required for AzureRM provider 2.x. 
+    # If you're using version 1.x, the "features" block is not allowed.
+    version = "~>2.0"
+    features {}
+    alias = "azure1"
+}
+
+provider "azurerm" {
+    # The "feature" block is required for AzureRM provider 2.x. 
+    # If you're using version 1.x, the "features" block is not allowed.
+    version = "~>2.0"
+    features {}
+    alias = "azure2"
+}
+
+# Create a resource group if it doesn't exist
+resource "azurerm_resource_group" "myterraformgroup1" {
+    name     = var.rg_name1
+    location = "eastus"
+    provider            = azurerm.azure1
+}
+
+# Create virtual network
+resource "azurerm_virtual_network" "myterraformnetwork1" {
+    name                = "myVnet1"
+    address_space       = ["10.0.0.0/16"]
+    location            = "eastus"
+    resource_group_name = azurerm_resource_group.myterraformgroup1.name
+}
+
+# Create a resource group if it doesn't exist
+resource "azurerm_resource_group" "myterraformgroup2" {
+    name     = var.rg_name2
+    location = "westus2"
+    provider            = azurerm.azure2
+}
+
+# Create virtual network
+resource "azurerm_virtual_network" "myterraformnetwork2" {
+    name                = "myVnet2"
+    location            = "westus2"
+    address_space       = ["10.0.0.0/16"]
+    resource_group_name = azurerm_resource_group.myterraformgroup2.name
+}
