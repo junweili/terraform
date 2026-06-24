19e8ef610bd19247f0db7e1e7e3f6854110abd0b
Junwei Li <junweili@users.noreply.github.com>
2026-06-23 22:51:46 -0700

    Update main.tf

--- template2/main.tf
+++ template2/main.tf
@@ -14,7 +14,7 @@ variable "department" {
 resource "aws_instance" "machine1" {
     ami           = "ami-04b9e92b5572fa0d1"
     instance_type = "t2.micro"
-    availability_zone = "us-east-1a"
+    availability_zone = "us-east-1f"
     subnet_id = "subnet-064590281af9c10f1"
 
     tags = {
@@ -25,7 +25,7 @@ resource "aws_instance" "machine1" {
 resource "aws_instance" "machine2" {
     ami           = "ami-04b9e92b5572fa0d1"
     instance_type = "t2.micro"
-    availability_zone = "us-east-1a"
+    availability_zone = "us-east-1f"
     subnet_id = "subnet-064590281af9c10f1"
 
     tags = {
